828bde02f12600765067aa2acafdfad74364282f
Alvin Pratama <113691381+palveeen22@users.noreply.github.com>
2024-11-13 23:26:00 +0300

    feat: type added

---
+++ types/index.tsx
@@ -0,0 +1,10 @@
+export type TInformationItem = {
+    status: string;
+    experience: string;
+    profession: string;
+    location: string;
+    country: string;
+    likes: number;
+    comments: number;
+    isSubscribed: boolean;
+  }
