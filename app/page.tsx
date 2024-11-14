f9c8065f0063e32d6e913e33018914929a29f9be
Alvin Pratama <113691381+palveeen22@users.noreply.github.com>
2024-11-14 12:58:29 +0300

    fix: fixing page entry

--- app/page.tsx
+++ app/page.tsx
@@ -1,17 +1,22 @@
-
+import HeaderBar from "@/components/HeaderBar";
 import ContentSide from "@/components/Home/ContentSide";
 import ImageContentSide from "@/components/Home/ImageContentSide";
 import SideBar from "@/components/Home/SideBar";
-import { sideBarData, BreadcrumbData } from "@/data"
+import { sideBarData, BreadcrumbData, headerBarData } from "@/data"
 
 
 export default function Home() {
 
   return (
-    <section className="w-full flex justify-between gap-8 md:gap-20 flex-grow">
-      <SideBar list={sideBarData} />
-      <ContentSide list={BreadcrumbData} />
-      <ImageContentSide/>
+    <section className="flex flex-col justify-between w-full h-full">
+      <section className="w-full">
+      <HeaderBar items={headerBarData} />
+      </section>
+      <section className="w-full flex justify-between gap-8 md:gap-20 flex-grow">
+        <SideBar list={sideBarData} />
+        <ContentSide list={BreadcrumbData} />
+        <ImageContentSide />
+      </section>
     </section>
   );
 }
