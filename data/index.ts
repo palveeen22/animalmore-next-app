506c04e0d64bc6a82e22b75b9b3496486fbaec94
Alvin Pratama <113691381+palveeen22@users.noreply.github.com>
2024-11-14 12:59:04 +0300

    update: mook data ummy update

---
+++ data/index.ts
@@ -0,0 +1,27 @@
+const headerBarData = ["Собаки", "Кошки", "Другие животные",  "Стримы",  "Сервисы", "Материалы", "Зоотовары"]
+const sideBarData = ["Заводчик", "Питомцы", "Пометы", "Блоги", "Вопросы", "Выставки", "Отзывы"]
+const BreadcrumbData = ["Главная", "Заводчики", "Марина Гриндер"]
+const navProfile = {
+    name:"Инесса",
+    profile: "https://s3-alpha-sig.figma.com/img/6267/26b2/be2595433c28eda2a913cea75a86a8c4?Expires=1732492800&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=Qo25mH5KrmS-RY7DvnYOryLXDBbQxkKSgZY7hg75qijEyfG8dJ4kyPPfbqbXa8AabX3I96noMKweX-YSqRSW-xWt9bkZ1msjalzRNYpf0JGHPVHHlaaLewwAh8aWR0T7CrFWLPicgHH2i4~IVV~ByrK9IEI2ANLOMO1scXaD9Q8ccdZ3imcmizlcCWNdWEUs4UBX5xeUW-wjseVK0xkOO1ljtZOQsR0Brcyd1896psWbcPI-2rZPpoTPfWqFpAWp~w8avVkD8ISBYIMYbK8RKN2hYVdDb6xO54BwesGDlldT2H2vDEkuGfJ88Y~zsrloiTlEw8vweuJhhGqORc0H-Q__"
+}
+
+const TextParagraph = "Марина Гриндер — заводчик болонок с многолетним опытом, известная своим профессионализмом и трепетным отношением к каждому питомцу. Ее питомник специализируется на разведении высокопородных болонок, соответствующих международным стандартам. Марина уделяет особое внимание здоровью, социализации и правильному воспитанию щенков с самого раннего возраста."
+
+const informationData = [{
+    status: "Заводчик гуру",
+    experience: "Опыт 8 лет",
+    profession: "кинолог",
+    location: "Химки",
+    country: "Москва",
+    likes: 24,
+    comments: 5,
+    isSubscribed: true
+}]
+
+const NoteAlert= "Заводчик свяжется с вами в удобное для вас время"
+
+const footerData = ["© 2020 – 2024 Сервис подбора питомцев", "ООО «Энималмор». Все права защищены.", "О компании", "Контакты", "Правовые документы", "Сотрудничество"]
+
+
+export { sideBarData, BreadcrumbData, TextParagraph, informationData, NoteAlert, footerData, headerBarData, navProfile }
